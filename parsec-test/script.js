c7611fcb979e0f42a1cefc09079a4deab5588a6a
jonshin <jonathanjshin@gmail.com>
2017-06-25 23:01:47 -0400

    parsec-test
    parsec-test

---
+++ parsec-test/script.js
@@ -0,0 +1,18 @@
+function change(){
+	ideal.src = "img/cars/black.jpg"; 
+}
+function change2(){
+	ideal.src = "img/cars/purple.jpg"; 
+}
+function change3(){
+	ideal.src = "img/cars/red.jpg"; 
+}
+function change4(){
+	ideal.src = "img/cars/green.jpg"; 
+}
+function change5(){
+	ideal.src = "img/cars/brown.jpg"; 
+}
+function change6(){
+	ideal.src = "img/cars/cyan.jpg"; 
+}
